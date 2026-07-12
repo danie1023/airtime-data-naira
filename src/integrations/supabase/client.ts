a4e0c533472087be02055ed8f50fe29cc4ef8256
danie1023 <kelvindaniel1022@gmail.com>
2026-07-12 04:34:05 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,11 @@
-import * as Supabase from '@supabase/supabase-js'
+sb_publishable_HnuCmFtpYjEdu_Eoab9WzQ_n5k_WqcP
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://gjenxbnbgjqkcjmnvqyu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqZW54Ym5iZ2pxa2NqbW52cXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDAwNTcsImV4cCI6MjA5OTE3NjA1N30.XuFXNXdNze0m2dILb7P0i41KEFQJ3EFnKAeq5vV7ZmU';
 
-export const supabase = Supabase.createClient(supabaseUrl, supabaseAnonKey)
+const supabaseAnonKey = 'sb_...'; // paste your full sb_ key here
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
